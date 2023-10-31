9d7ad701b9c328c216f7dec3705baed114c809e8
Mathéo DELAUNAY <dseonay@gmail.com>
2023-10-31 09:36:08 +0100

    add : margin footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import {blue} from "@sanity/color";
 
 const Footer = () => {
     return (
@@ -8,15 +9,15 @@ const Footer = () => {
             <div className="justify-between flex flex-row">
                 {/* Section des Liens de Navigation */}
                 <div className="flex-col justify-center items-center">
-                    <div className="text-center text-[22px] font-bold">Pages</div>
-                    <div className="text-center text-xl">Home</div>
-                    <div className="text-center text-xl">About me</div>
-                    <div className="text-center text-xl">Projects</div>
-                    <div className="text-center text-xl">Contact</div>
+                    <div className="text-center text-[22px] font-bold my-3">Pages</div>
+                    <div className="text-center text-xl my-2">Home</div>
+                    <div className="text-center text-xl my-2">About me</div>
+                    <div className="text-center text-xl my-2">Projects</div>
+                    <div className="text-center text-xl my-2">Contact</div>
                 </div>
                 {/* Section d'Abonnement à la Newsletter */}
                 <div className="flex-col justify-center items-center">
-                    <div className="text-center text-[22px] font-bold">Newsletter</div>
+                    <div className="text-center text-[22px] font-bold my-3">Newsletter</div>
                     <div className="text-center text-xl">S'abonner</div>
                 </div>
                 {/* Section des Informations de Copyright */}
@@ -26,52 +27,55 @@ const Footer = () => {
                 </div>
                 {/* Section des Liens vers les Projets */}
                 <div className="flex-col justify-center items-center ">
-                    <div className="text-center text-[22px] font-bold">Projects</div>
-                    <div className="text-center text-xl">Project 1</div>
-                    <div className="text-center text-xl">Project 2</div>
-                    <div className="text-center text-xl">Project 3</div>
+                    <div className="text-center text-[22px] font-bold my-3">Projects</div>
+                    <div className="text-center text-xl my-2">Project 1</div>
+                    <div className="text-center text-xl my-2">Project 2</div>
+                    <div className="text-center text-xl my-2">Project 3</div>
                 </div>
                 {/* Section des Liens Légaux */}
                 <div className="flex-col justify-center items-center ">
-                    <div className="text-center text-[22px] font-bold">Mentions Légales</div>
-                    <div className="text-center text-xl">Politique de confidentialité</div>
-                    <div className="text-center text-xl">FAQ</div>
+                    <div className="text-center text-[22px] font-bold my-3">Mentions Légales</div>
+                    <div className="text-center text-xl my-2">Politique de confidentialité</div>
+                    <div className="text-center text-xl my-2">FAQ</div>
                 </div>
             </div>
-            <div className="flex justify-center items-center ">
-                <Link
-                    href="https://www.linkedin.com/in/math%C3%A9o-delaunay/"
+            <div className="flex justify-center items-center">
+                <div className="mx-20">
+                    <Link
+                        href="https://www.linkedin.com/in/math%C3%A9o-delaunay/"
+                        target="_blank">
+                        <Image
+                            src="/logo-linkedin.svg"
+                            alt="Logo-linkedin"
+                            width={40}
+                            height={40}
+                        />
+                    </Link>
+                </div>
+                <div className="mx-20">
+                    <Link className="white hover:text-blue-500"
+                        href="https://github.com/D-Seonay"
+                        target="_blank">
+                        <Image
+                            src="/logo-github.svg"
+                            alt="Logo-github"
+                            width={40}
+                            height={40}
+                        />
+                    </Link>
+                </div>
+                <div className="mx-20">
+                    <Link
+                    href="matheodelaunay04@gmail.com"
                     target="_blank">
                     <Image
-                        src="/logo-linkedin.svg"
-                        alt="Logo-linkedin"
+                        src="/logo-mail.svg"
+                        alt="Logo-mail"
                         width={40}
                         height={40}
                     />
-                </Link>
-                <Link
-                    href="https://github.com/D-Seonay"
-                    target="_blank">
-                    <Image
-                        src="/logo-github.svg"
-                        alt="Logo-github"
-                        width={40}
-                        height={40}
-                    />
-                </Link>
-
-                <Link
-                href="matheodelaunay04@gmail.com"
-                target="_blank">
-                <Image
-                    src="/logo-mail.svg"
-                    alt="Logo-mail"
-                    width={40}
-                    height={40}
-                />
-            </Link>
-                <div className="w-66 h-16 relative" />
-                <div className="w-65 h-55 relative" />
+                    </Link>
+                </div>
             </div>
         </div>
     );
