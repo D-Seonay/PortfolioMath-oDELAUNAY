80947963a6412e0fd1bf4070359bb8b6ab2e5512
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-02 15:30:54 +0100

    add "icons skills cards"

--- components/sections/SkillsPage.tsx
+++ components/sections/SkillsPage.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 interface SkillCategoryProps {
     title: string;
     description: string;
+    icon: string;
 }
 
-const SkillCategory: React.FC<SkillCategoryProps> = ({ title, description }) => {
+const SkillCategory: React.FC<SkillCategoryProps> = ({ title, description, icon }) => {
     return (
-        <div className="w-72 h-96 p-5 bg-zinc-900 rounded-lg border-2 border-neutral-800 flex flex-col justify-end items-start space-y-5">
-            <div className="w-20 h-20 relative" />
+        <div className="w-72 h-96 p-5 bg-zinc-900 rounded-md border-2 border-neutral-800 flex flex-col justify-end items-start space-y-5 hover:border-primary">
+            <img src={icon} width={40} height={40} alt={title} />
             <div className="text-center text-gray-400 text-3xl font-normal font-LilitaOne">{title}</div>
             <div className="text-gray-400 text-xl font-normal font-LilitaOne">{description}</div>
         </div>
@@ -20,10 +21,10 @@ const SkillsPage: React.FC = () => {
         <div className="w-full h-auto p-5 relative">
             <h1 className="absolute left-0 top-0 text-6xl font-black font-Poppins">SKILLS</h1>
             <div className="flex justify-between space-x-5">
-                <SkillCategory title="FRONT END" description="Designing intuitive interfaces" />
-                <SkillCategory title="BACK END" description="Building robust server-side applications" />
-                <SkillCategory title="DEVTOOLS" description="Working with development tools" />
-                <SkillCategory title="CREATIVE" description="Expressing creativity in design" />
+                <SkillCategory title="FRONT END" description="Designing intuitive interfaces" icon="../FrontEnd.svg"/>
+                <SkillCategory title="BACK END" description="Building robust server-side applications" icon='../FrontEnd.svg' />
+                <SkillCategory title="DEVTOOLS" description="Working with development tools" icon='./FrontEnd.svg'/>
+                <SkillCategory title="CREATIVE" description="Expressing creativity in design" icon=''/>
             </div>
         </div>
     );
