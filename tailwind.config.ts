b8642b74f8ce78f43b18c0a87ca1eba8ec4610c3
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-02 16:43:25 +0100

    Refactor tailwind.config.js to use windicss in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,84 +1,98 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ["class"],
+// tailwind.config.ts
+
+import { defineConfig } from 'windicss/helpers';
+
+export default defineConfig({
+  darkMode: 'class',
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
     screens: {
-      xs: "400px",
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1220px",
-      "2xl": "1440px",
-      "3xl": "1700px",
+      xs: '400px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1220px',
+      '2xl': '1440px',
+      '3xl': '1700px',
     },
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
+        '2xl': '1400px',
       },
     },
     extend: {
       fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-        inter: ["Inter", "sans-serif"],
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
       },
       border: {
-        1: "1px solid",
-        2: "2px solid",
-        3: "3px solid",
-        radius: "10px",
+        1: '1px solid',
+        2: '2px solid',
+        3: '3px solid',
+        radius: '10px',
       },
       colors: {
-        primary: "#2163AD",
-        secondary: "#1A4F8A",
+        primary: '#2163AD',
+        secondary: '#1A4F8A',
         black: {
-          DEFAULT: "#000",
-          100: "#191919",
-          200: "#161B22",
-          300: "#1F2428",
-          400: "#242C38",
+          DEFAULT: '#000',
+          100: '#191919',
+          200: '#161B22',
+          300: '#1F2428',
+          400: '#242C38',
         },
         grey: {
-          DEFAULT: "#B3BBC3",
-          100: "#969BA5",
-          200: "#55616D",
+          DEFAULT: '#B3BBC3',
+          100: '#969BA5',
+          200: '#55616D',
         },
         white: {
-          DEFAULT: "#FFF",
-          400: "#A3B3BC",
-          500: "#A4B8D5",
-          800: "#D0DFFF",
+          DEFAULT: '#FFF',
+          400: '#A3B3BC',
+          500: '#A4B8D5',
+          800: '#D0DFFF',
         },
-        purple: "#8C7CFF",
-        pink: "#ED5FBD",
-        violet: "#F16565",
-        orange: "#FF964B",
+        blue: {
+          default: '#2163AD',
+          100: '#E6F0FF',
+          200: '#CCE0FF',
+          300: '#B3D1FF',
+          400: '#99C2FF',
+          500: '#80B3FF',
+          600: '#66A3FF',
+          700: '#4D94FF',
+          800: '#3385FF',
+          purple: '#8C7CFF',
+        },
+        pink: '#ED5FBD',
+        violet: '#F16565',
+        orange: '#FF964B',
       },
       backgroundImage: {
         banner: "url('/jsm_resources_banner.svg')",
       },
       keyframes: {
-        "accordion-down": {
+        'accordion-down': {
           from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: 0 },
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-};
+  plugins: [require('tailwindcss-animate')],
+});
