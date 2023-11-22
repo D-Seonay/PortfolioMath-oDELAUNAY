7bbbde8050ccc90ad176644c58571876d07e4796
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-22 12:46:42 +0100

    Add Navbar component to BurgerMenu

--- components/BurgerMenu.tsx
+++ components/BurgerMenu.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import Link from "next/link";
+import Navbar from "./Navbar";
 
 const BurgerMenu = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -11,13 +12,14 @@ const BurgerMenu = () => {
     };
 
     return (
+    
         <div className="lg:hidden">
             <button
                 onClick={toggleMenu}
-                className={`fixed top-6 right-6 z-50 p-2 bg-white rounded-full ${isOpen ? "hidden" : ""}`}
+                className={`fixed top-6 right-6 z-50 p-2 bg-black-100 rounded-full ${isOpen ? "hidden" : ""}`}
             >
                 <svg
-                    className="w-6 h-6 text-gray-900"
+                    className="w-6 h-6 text-gray-100"
                     fill="none"
                     stroke="currentColor"
                     viewBox="0 0 24 24"
@@ -32,17 +34,17 @@ const BurgerMenu = () => {
                 </svg>
             </button>
             <div
-                className={`fixed top-0 left-0 w-full h-full bg-white bg-opacity-95 p-6 text-gray-900 transform transition-transform duration-300 ease-in-out ${
+                className={`fixed top-0 left-0 w-full h-full bg-black-100 p-6 text-white transform transition-transform duration-300 ease-in-out ${
                     isOpen ? "translate-x-0" : "-translate-x-full"
                 }`}
             >
                 <div className="flex justify-end">
                     <button
                         onClick={toggleMenu}
-                        className="p-2 bg-gray-200 rounded-full"
+                        className="p-2 bg-sky-700 rounded-full"
                     >
                         <svg
-                            className="w-6 h-6 text-gray-900"
+                            className="w-6 h-6 text-white"
                             fill="none"
                             stroke="currentColor"
                             viewBox="0 0 24 24"
@@ -57,20 +59,22 @@ const BurgerMenu = () => {
                         </svg>
                     </button>
                 </div>
-                <ul className="mt-10 space-y-4">
-                    <li>
-                        <Link href="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link href="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link href="/projects">Projects</Link>
-                    </li>
-                    <li>
-                        <Link href="/contact">Contact</Link>
-                    </li>
-                </ul>
+
+                <div className="flex-col flex-center flex-colmt-10 space-y-8 text-xl font-normal">
+
+                    <div>
+                        <Link href="/">HOME</Link>
+                    </div>
+                    <div>
+                        <Link href="/about">ABOUT ME</Link>
+                    </div>
+                    <div>
+                        <Link href="/projects">PROJECTS</Link>
+                    </div>
+                    <div>
+                        <Link href="/contact">CONTACT</Link>
+                    </div>
+                </div>
             </div>
         </div>
     );
