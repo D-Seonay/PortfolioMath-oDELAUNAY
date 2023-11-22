e1a9a85575a4fe559e3853dd4c482983c430bf44
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-22 12:26:01 +0100

    Remove footer and update navbar layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,30 +10,34 @@ const Navbar = () => {
                     className="text-sky-700 text-2xl font-extrabold">SEONAY.
                 </Link>
 
-                <BurgerMenu />
-
-                <div className="justify-center items-center inline-flex">
-                    <Link
-                        href="/"
-                        className="text-white text-xl font-normal mx-2">HOME
-                    </Link>
-                    <span className="text-zinc-900 text-xl font-normal mx-2">/</span>
-                    <Link
-                        href="#about-page"
-                        className="text-white text-xl font-normal mx-2">ABOUT ME
-                    </Link>
-                    <span className="text-zinc-900 text-xl font-normal mx-2">/</span>
-                    <Link
-                        href="#projects-page"
-                        className="text-white text-xl font-normal mx-2">PROJECTS
-                    </Link>
+                <div className="md:hidden">
+                    <BurgerMenu />
                 </div>
-                <div className="py-2.5 rounded-[50px] border-2 border-black justify-center items-center gap-[30px] inline-flex">
-                    <Link
-                        href="#contact-page"
-                        className="text-sky-700 text-xl font-extrabold px-4 py-1">CONTACT ME
-                    </Link>
+                <div className="hidden md:inline-flex">
+                    <div className="justify-center items-center ">
+                        <Link
+                            href="/"
+                            className="text-white text-xl font-normal mx-2">HOME
+                        </Link>
+                        <span className="text-zinc-900 text-xl font-normal mx-2">/</span>
+                        <Link
+                            href="#about-page"
+                            className="text-white text-xl font-normal mx-2">ABOUT ME
+                        </Link>
+                        <span className="text-zinc-900 text-xl font-normal mx-2">/</span>
+                        <Link
+                            href="#projects-page"
+                            className="text-white text-xl font-normal mx-2">PROJECTS
+                        </Link>
+                    </div>
+                    
                 </div>
+                <div className="py-2.5 rounded-[50px] border-2 border-black justify-center items-center gap-[30px] hidden md:inline-flex">
+                        <Link
+                            href="#contact-page"
+                            className="text-sky-700 text-xl font-extrabold px-4 py-1">CONTACT ME
+                        </Link>
+                    </div>
             </div>
         </nav>
     );
