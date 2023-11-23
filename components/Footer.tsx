a226a463ee863659b418006b081af03c257cd47e
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-23 09:46:25 +0100

    Refactor footer component styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,26 +3,26 @@ import Link from "next/link";
 
 const Footer = () => {
     return (
-        <div className="w-full bg-neutral-800 p-10 text-white absolute">
-            <div className="justify-between flex flex-row">
+        <div className="w-full flex-row bg-neutral-800 p-10 text-white absolute">
+            <div className="justify-between flex flex-col md:flex-row">
                 {/* Section des Liens de Navigation */}
-                <div className="flex-col justify-center items-center">
+                <div className="flex-col justify-center items-center m-4 p-4 md:m-0 mb:p-0">
                     <div className="text-center text-[22px] font-bold my-3">Pages</div>
                     <div className="text-center text-xl my-2">
                         <Link href="/" title="Go to Home page">Home</Link>
                     </div>
-                    <div className="text-center text-xl my-2">
+                    <div className="text-center text-xl my-2 m-4 p-4 md:m-0 md:0">
                         <Link href="/about" title="Go to About page">About me</Link>
                     </div>
-                    <div className="text-center text-xl my-2">
+                    <div className="text-center text-xl m-4 p-4 md:m-0 md:0">
                         <Link href="/projects" title="Go to Projects page">Projects</Link>
                     </div>
-                    <div className="text-center text-xl my-2">
+                    <div className="text-center text-xl m-4 p-4 md:m-0 md:0">
                         <Link href="/contact" title="Go to Contact page">Contact</Link>
                     </div>
                 </div>
                 {/* Section d'Abonnement à la Newsletter */}
-                <div className="flex-col justify-center items-center">
+                <div className="flex-col justify-center items-center my-2 m-4 p-4 md:m-0 md:0">
                     <div className="text-center text-[22px] font-bold my-3">Newsletter</div>
                     <div className="text-center text-xl">
                         <Link href="/newsletter" title="Subscribe to Newsletter">S &apos; abonner</Link>
@@ -39,7 +39,7 @@ const Footer = () => {
                 </div>
 
                 {/* Section des Liens vers les Projets */}
-                <div className="flex-col justify-center items-center ">
+                <div className="flex-col justify-center items-center my-2 m-4 p-4 md:m-0 md:0">
                     <div className="text-center text-[22px] font-bold my-3">Projects</div>
                     <div className="text-center text-xl my-2">
                         <Link href="/project1" title="Go to Project 1 page">Project 1</Link>
@@ -52,7 +52,7 @@ const Footer = () => {
                     </div>
                 </div>
                 {/* Section des Liens Légaux */}
-                <div className="flex-col justify-center items-center ">
+                <div className="flex-col justify-center items-center my-2 m-4 p-4 md:m-0 md:0">
                     <div className="text-center text-[22px] font-bold my-3">Mentions Légales</div>
                     <div className="text-center text-xl my-2">
                         <Link href="/privacy-policy" title="Go to Privacy Policy page">Politique de confidentialité</Link>
