d9fc4678a631502f21818f4941b35b9542590177
Mathéo DELAUNAY <dseonay@gmail.com>
2023-12-03 15:13:17 +0100

    Add Hero component and update layout

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,15 +3,16 @@ import React from "react";
 import { ContactPage } from "@/components/sections/ContactPage";
 import { SkillsPage } from "@/components/sections/SkillsPage";
 import {AboutMePage} from "@/components/sections/AboutMePage";
+import { HomePage } from "@/components/sections/HomePage";
+import { Hero } from "@/components/sections/Hero";
 
 const Page = () => {
     return (
         <div className="relative bg-gray-400">
             <div className="absolute bg-zinc-900 left-0 top-[798px] w-full h-[1208px]"></div>
 
-        <SkillsPage/>
+            <Hero />
         </div>
-        
 
     );
 };
--- components/sections/SkillsPage.tsx
+++ components/sections/SkillsPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 interface SkillCategoryProps {
     title: string;
@@ -9,7 +10,7 @@ interface SkillCategoryProps {
 const SkillCategory: React.FC<SkillCategoryProps> = ({ title, description, icon }) => {
     return (
         <div className="w-72 h-96 p-5 bg-black-100 rounded-md border-2 border-neutral-800 flex flex-col items-start space-y-5 hover:border-secondary m-9">
-            <img src={icon} width={40} height={40} alt={title} />
+            <Image src={icon} width={40} height={40} alt={title} />
             <div className="text-center text-gray-400 text font-normal font-LilitaOne">{title}</div>
             <div className="text-gray-400 text-xl font-normal font-LilitaOne">{description}</div>
         </div>
@@ -20,6 +21,7 @@ const SkillsPage: React.FC = () => {
     return (
         <div className="w-full h-auto p-5 relative flex flex-col items-center">
             <h1 className="absolute left-0 top-0 text-4xl font-black font-Poppins text-black text-opacity-10 text-stroke-500 ">SKILLS</h1>
+            <div className="absolute left-0 top-0 w-full h-full flex justify-center items-center text-white text-9xl font-black font-Poppins text-opacity-10 text-stroke-500 ">SEONAY</div>
             <div className="flex flex-col md:flex-row justify-between space-x-5">
                 <SkillCategory title="FRONT END" description="Designing intuitive interfaces" icon="../FrontEnd.svg"/>
                 <SkillCategory title="BACK END" description="Building robust server-side applications" icon='../BackEnd.svg' />
