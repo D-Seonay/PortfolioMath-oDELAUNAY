f98009b752268625a960082df7c1c082e7cd3fce
Mathéo DELAUNAY <dseonay@gmail.com>
2024-01-19 10:11:16 +0100

    Add icons to SkillCategory component

--- components/sections/SkillsPage.tsx
+++ components/sections/SkillsPage.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Image from 'next/image';
+import { IoPhonePortraitOutline } from "react-icons/io5";
+import { HiTerminal } from "react-icons/hi";
+import { RiToolsFill } from "react-icons/ri";
+import { FaPaintRoller } from "react-icons/fa6";
+
+
+
 
 interface SkillCategoryProps {
     title: string;
     description: string;
-    icon: string;
+    icon: React.ReactNode;
 }
 
 const SkillsPage: React.FC = () => {
@@ -14,10 +21,11 @@ const SkillsPage: React.FC = () => {
             <div className="text-[6em] md:text-[128px]  w-full font-bold text-transparent text-center md:text-left" style={{ WebkitTextStroke: '1px white' }}>SKILLS</div>
             <div className='flex flex-col items-center justify-center'>
                 <div className="flex flex-col md:flex-row justify-between items-center">
-                    <SkillCategory title="FRONT END" description="Designing intuitive interfaces" icon="../FrontEnd.svg" />
-                    <SkillCategory title="BACK END" description="Building robust server-side applications" icon="../BackEnd.svg" />
-                    <SkillCategory title="DEVTOOLS" description="Working with development tools" icon="./DevTools.svg" />
-                    <SkillCategory title="CREATIVE" description="Expressing creativity in design" icon="./Creative.svg" />
+
+                    <SkillCategory title="FRONT END" description="Designing intuitive interfaces" icon={<IoPhonePortraitOutline />} />
+                    <SkillCategory title="BACK END" description="Developing server-side applications" icon={<HiTerminal />} />
+                    <SkillCategory title="DEVTOOLS" description="Working with development tools" icon={<RiToolsFill />} />
+                    <SkillCategory title="CREATIVE" description="Expressing creativity in design" icon={<FaPaintRoller />}/>
                 </div>
             </div>
         </div>
@@ -27,7 +35,7 @@ const SkillsPage: React.FC = () => {
 const SkillCategory: React.FC<SkillCategoryProps> = ({ title, description, icon }) => {
     return (
         <div className="w-full md:w-1/4 h-96 p-5 bg-black-100 rounded-2xl border-4 border-neutral-800 flex flex-col items-start space-y-5 hover:border-secondary my-5 md:m-9 transition duration-300 ease-in-out transform hover:scale-105">
-            <Image src={icon} width={40} height={40} alt={title} />
+            <div className="text-center text-5xl text-gray-400">{icon}</div>
             <div className="text-center text-gray-400 text font-normal">{title}</div>
             <div className="text-gray-400 text-xl font-normal">{description}</div>
         </div>
