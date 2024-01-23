1ed68af06ed11234226b156563c934084ee3eac4
Mathéo DELAUNAY <dseonay@gmail.com>
2024-01-23 13:50:43 +0100

    Add project title styling

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -6,6 +6,7 @@ import { projectsData } from '@/lib/data';
 const Projects: React.FC = () => {
   return (
     <div className="w-full h-full p-5 relative flex flex-col items-center">
+      <div className="text-[6em] md:text-[128px]  w-full font-bold text-transparent text-center md:text-left" style={{ WebkitTextStroke: '1px white' }}>PROJECT</div>
       <div className='flex flex-col items-center justify-center'>
         <div className="flex flex-col md:flex-row justify-between items-center">
           {projectsData.map((project, index) => (
