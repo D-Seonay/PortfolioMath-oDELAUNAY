ac6471253f33f319b955d6d40142594a8689ca8c
Mathéo DELAUNAY <dseonay@gmail.com>
2024-02-01 14:44:45 +0100

    Add social media icons and contact buttons to AboutMePage

--- components/sections/AboutMePage.tsx
+++ components/sections/AboutMePage.tsx
@@ -1,5 +1,15 @@
+"use client";
+
 import React from "react";
 import { Hero } from "@/components/sections/Hero";
+import { motion } from "framer-motion";
+import { SiGoogledocs } from "react-icons/si";
+import { MdEmail } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa6";
+import { GrGithub } from "react-icons/gr";
+
+
+
 
 const AboutMePage = () => {
     return (
@@ -12,9 +22,57 @@ const AboutMePage = () => {
                 <div className="text-center text-white text-[25px] flex flex-col items-start">
                     <div className="text-center text-white text-[25px] md:text-[40px] text-bold ">—ABOUT ME—</div>
                     <div className=" text-white text-[15px] md:text-[20px] text-start p-3">I &apos; Mathéo DELAUNAY, a computer engineering student at EPSI Nantes. Passionate about programming and computing in general, I &apos; ve acquired skills in HTML, CSS, PHP, MySQL and Adobe suites. I also enjoy mountain sports and video games. Check out my portfolio to find out more about my skills and achievements.</div>
-                    <button className="text-center text-white text-[15px] md:text-[25px] flex flex-col items-start transition duration-300 ease-in-out transform hover:scale-105 hover:bg-primary hover:border hover:border-transparent hover:shadow-lg mt-4 px-6 py-3 border rounded-lg border-primary">
-                        <div className="text-center text-white text-[15px] md:text-[25px] text-bold">CONTACT ME</div>
-                    </button>
+                    <div className="flex p-4 ">
+                        <motion.button
+                            className={`group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-primary text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-secondary active:scale-105 dark:bg-white dark:bg-opacity-10 mx-3`}>
+                                <a href="/docs/cv.pdf"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    about="CV"  
+                                    title="Go to CV page" 
+                                >
+                                    <SiGoogledocs className="text-s opacity-100 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
+                                </a>
+                        </motion.button>
+                        <motion.button
+                            className={`group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-primary text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-secondary active:scale-105 dark:bg-white dark:bg-opacity-10 mx-3`}>
+                                <a href="mailto:matheodelaunay04@gmail.com"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    about="Email"
+                                    title="Go to Email page"
+                                >
+                                    <MdEmail className="text-s opacity-100 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
+                                </a> 
+                        </motion.button>
+
+                        <motion.button
+                            className={`group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-primary text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-secondary active:scale-105 dark:bg-white dark:bg-opacity-10 mx-3`}>
+                                <a href="https://www.linkedin.com/in/math%C3%A9o-delaunay/"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    about="LinkedIn"
+                                    title="Go to LinkedIn page"
+                                >
+                                    <FaLinkedin className="text-s opacity-100 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
+                                </a> 
+                        </motion.button>
+
+                        <motion.button
+                            className={`group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-primary text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-secondary active:scale-105 dark:bg-white dark:bg-opacity-10 mx-3`}>
+                                <a href="https://github.com/D-Seonay"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    about="Github"
+                                    title="Go to GitHub page"
+                                >
+                                    <GrGithub className="text-s opacity-100 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
+                                </a>
+                        </motion.button>
+                    </div>
+
+
+
                 </div>
             </div>
             
