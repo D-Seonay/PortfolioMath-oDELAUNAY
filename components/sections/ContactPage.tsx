8b34e4634485830c773def9388a54d69c26bdbfc
Mathéo DELAUNAY <dseonay@gmail.com>
2024-02-01 13:23:43 +0100

    Add form validation and animation to ContactPage component

--- components/sections/ContactPage.tsx
+++ components/sections/ContactPage.tsx
@@ -1,60 +1,88 @@
 "use client";
-
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 import { FaPaperPlane } from "react-icons/fa";
-
-
+import { motion, useViewportScroll, useTransform } from "framer-motion";
 
 function ContactForm() {
   const [state, handleSubmit] = useForm("meqyegyw");
+  const { scrollYProgress } = useViewportScroll();
+  const [isFilled, setIsFilled] = useState({
+    firstname: false,
+    lastname: false,
+    email: false,
+    message: false,
+  });
+
+  const yProgress = useTransform(scrollYProgress, [0, 1], ['45vh', '0vh']);
+
   if (state.succeeded) {
-    return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <h1 className="text-3xl font-bold">Thanks for your message!</h1>
-      <p className="text-lg">I &apos; ll get back to you as soon as possible.</p>
-      </div>
-    ); 
+    return <p className="text-center text-primary text-xl font-bold">Thanks for contacting me!</p>;
+    
   }
-  else if (state.errors) {
-    return (
-      <div className="flex flex-col items-center justify-center gap-4">
-      <h1 className="text-3xl font-bold">Something went wrong.</h1>
-      <p className="text-lg">Please try again later.</p>
-      </div>
-    );
-  }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setIsFilled((prevState) => ({
+      ...prevState,
+      [name]: value.trim() !== "",
+    }));
+  };
+
   return (
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="firstname">
+    <div className="w-full h-full p-5 relative flex flex-col items-center">
+      <div className="text-[6em] md:text-[128px]  w-full font-bold text-transparent text-center md:text-left" style={{ WebkitTextStroke: '1px white' }}>CONTACT</div>
+    <motion.form
+    
+      className="mb-3 sm:mb-8 grid-cols-1 md:grid-cols-2 mx-5 md:mx-9  w-full md:w-2/3 h-auto p-3 border-4 border-neutral-800 rounded-[10px] "
+      style={{ y: yProgress }}
+      onSubmit={handleSubmit}
+    >
+      <h1 className="text-3xl font-bold text-center text-primary">Contact Me</h1>
+      <div className="flex flex-col md:flex-row justify-between">
+        <div className="flex flex-col gap-4 m-3 w-1/2">
+        <label htmlFor="firstname" className={`flex flex-col gap-1 text-lg text-primary font-bold ${isFilled.firstname ? 'block' : 'hidden'}`}>
           First Name
         </label>
         <input
           id="firstname"
-          type="firstname" 
+          type="text" 
           name="firstname"
+          placeholder='First Name'
           required
+          className="rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 p-2 w-1/9 md:w-full bg-white bg-opacity-10"
+          onChange={handleInputChange}
         />
         <ValidationError
           prefix="First name" 
           field="firstname"
           errors={state.errors}
         />
-        <label htmlFor="lastname">
+      </div>
+
+      <div className="flex flex-col gap-4 m-3 w-1/2">
+        <label htmlFor="lastname" className={`flex flex-col gap-1 text-lg text-primary font-bold ${isFilled.lastname ? 'block' : 'hidden'}`}>
           Last Name
         </label>
         <input
           id="lastname"
-          type="lastname" 
+          type="text" 
           name="lastname"
+          placeholder='Last Name'
           required
+          className="rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 p-2 w-1/9 md:w-full bg-white bg-opacity-10"
+          onChange={handleInputChange}
         />
         <ValidationError
           prefix="Last name" 
           field="lastname"
           errors={state.errors}
         />
-        <label htmlFor="email">
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <label htmlFor="email" className={`flex flex-col gap-1 text-lg text-primary font-bold ${isFilled.email ? 'block' : 'hidden'}`}>
           Email Address
         </label>
         <input
@@ -62,13 +90,19 @@ function ContactForm() {
           type="email" 
           name="email"
           required
+          placeholder='Email Address'
+          className="rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 p-2 m-3 bg-white bg-opacity-10"
+          onChange={handleInputChange}
         />
         <ValidationError 
           prefix="Email" 
           field="email"
           errors={state.errors}
         />
-        <label htmlFor="message">
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <label htmlFor="message" className={`flex flex-col gap-1 text-lg text-primary font-bold ${isFilled.message ? 'block' : 'hidden'}`}>
           Message
         </label>
         <textarea
@@ -76,21 +110,26 @@ function ContactForm() {
           name="message"
           placeholder='Message'
           required
+          style={{ resize: 'none' }}
+          className="rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 p-2 m-3 bg-white bg-opacity-10"
+          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => handleInputChange(e)}
         />
         <ValidationError 
           prefix="Message" 
           field="message"
           errors={state.errors}
         />
-        <button
-          type="submit"
-          className={`group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 ${
-            state.submitting
-              ? "cursor-not-allowed opacity-65"
-              : "hover:scale-110 hover:bg-gray-950 active:scale-105"
-          } dark:bg-white dark:bg-opacity-10`}
-          disabled={state.submitting}
-        >
+      </div>
+
+      <motion.button
+        type="submit"
+        className={`group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-primary text-white rounded-full outline-none transition-all focus:scale-110 ${
+          state.submitting
+            ? "cursor-not-allowed opacity-65"
+            : "hover:scale-110 hover:bg-secondary active:scale-105"
+        } dark:bg-white dark:bg-opacity-10`}
+        disabled={state.submitting}
+      >
         {state.submitting ? (
           <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white"></div>
         ) : (
@@ -99,13 +138,18 @@ function ContactForm() {
             <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
           </>
         )}
-      </button>
-    </form>
+      </motion.button>
+    </motion.form>
+    </div>
   );
 }
+
+
+
 function App() {
   return (
     <ContactForm />
   );
 }
+
 export default App;
