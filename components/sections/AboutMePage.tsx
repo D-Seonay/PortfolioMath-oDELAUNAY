772ed2d16e07ff5f677b24227a90d98968d31ac8
Mathéo DELAUNAY <dseonay@gmail.com>
2024-02-01 15:06:21 +0100

    Add tooltips to AboutMePage

--- components/sections/AboutMePage.tsx
+++ components/sections/AboutMePage.tsx
@@ -7,6 +7,10 @@ import { SiGoogledocs } from "react-icons/si";
 import { MdEmail } from "react-icons/md";
 import { FaLinkedin } from "react-icons/fa6";
 import { GrGithub } from "react-icons/gr";
+import { Tooltip } from 'react-tooltip'
+
+
+
 
 
 
@@ -14,6 +18,15 @@ import { GrGithub } from "react-icons/gr";
 const AboutMePage = () => {
     return (
         <div className="flex justify-center items-center flex-col">
+
+
+<Tooltip anchorSelect=".my-anchor-element-email" place="bottom">Email</Tooltip>
+<Tooltip anchorSelect=".my-anchor-element-cv" place="bottom">CV</Tooltip>
+<Tooltip anchorSelect=".my-anchor-element-link" place="bottom">LinkedIn</Tooltip>
+<Tooltip anchorSelect=".my-anchor-element-github" place="bottom">Github</Tooltip>
+
+
+
             <div className="text-center text-sky-700 text-xl font-extrabold font-Poppins">CULTIVATING QUIET ELEGANCE</div>
             <div className="flex flex-col md:flex-row items-center">
                 <div className="">
@@ -23,19 +36,20 @@ const AboutMePage = () => {
                     <div className="text-center text-white text-[25px] md:text-[40px] text-bold ">—ABOUT ME—</div>
                     <div className=" text-white text-[15px] md:text-[20px] text-start p-3">I &apos; Mathéo DELAUNAY, a computer engineering student at EPSI Nantes. Passionate about programming and computing in general, I &apos; ve acquired skills in HTML, CSS, PHP, MySQL and Adobe suites. I also enjoy mountain sports and video games. Check out my portfolio to find out more about my skills and achievements.</div>
                     <div className="flex p-4 ">
+                    <motion.button
+        className={`my-anchor-element-cv group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-primary text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-secondary active:scale-105 dark:bg-white dark:bg-opacity-10 mx-3`}
+        data-tip="CV"
+      >
+        <a href="/docs/cv.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Go to CV page"
+        >
+          <SiGoogledocs className="text-s opacity-100 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
+        </a>
+      </motion.button>
                         <motion.button
-                            className={`group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-primary text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-secondary active:scale-105 dark:bg-white dark:bg-opacity-10 mx-3`}>
-                                <a href="/docs/cv.pdf"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    about="CV"  
-                                    title="Go to CV page" 
-                                >
-                                    <SiGoogledocs className="text-s opacity-100 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
-                                </a>
-                        </motion.button>
-                        <motion.button
-                            className={`group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-primary text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-secondary active:scale-105 dark:bg-white dark:bg-opacity-10 mx-3`}>
+                            className={`my-anchor-element-email group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-primary text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-secondary active:scale-105 dark:bg-white dark:bg-opacity-10 mx-3`}>
                                 <a href="mailto:matheodelaunay04@gmail.com"
                                     target="_blank"
                                     rel="noopener noreferrer"
@@ -45,9 +59,10 @@ const AboutMePage = () => {
                                     <MdEmail className="text-s opacity-100 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
                                 </a> 
                         </motion.button>
+                        
 
                         <motion.button
-                            className={`group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-primary text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-secondary active:scale-105 dark:bg-white dark:bg-opacity-10 mx-3`}>
+                            className={`my-anchor-element-link group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-primary text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-secondary active:scale-105 dark:bg-white dark:bg-opacity-10 mx-3`}>
                                 <a href="https://www.linkedin.com/in/math%C3%A9o-delaunay/"
                                     target="_blank"
                                     rel="noopener noreferrer"
@@ -59,7 +74,7 @@ const AboutMePage = () => {
                         </motion.button>
 
                         <motion.button
-                            className={`group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-primary text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-secondary active:scale-105 dark:bg-white dark:bg-opacity-10 mx-3`}>
+                            className={`my-anchor-element-github group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-primary text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-secondary active:scale-105 dark:bg-white dark:bg-opacity-10 mx-3`}>
                                 <a href="https://github.com/D-Seonay"
                                     target="_blank"
                                     rel="noopener noreferrer"
