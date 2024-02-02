5dcd0b201a896752ba99d6bb493215ee8f86f9dd
Mathéo DELAUNAY <dseonay@gmail.com>
2024-02-02 21:40:37 +0100

    Refactor styling in Project and Skill components

--- components/sections/Skill.tsx
+++ components/sections/Skill.tsx
@@ -34,10 +34,12 @@ const SkillCategory: React.FC<SkillCategoryProps> = ({
         }}
         className="mb-3 sm:mb-8 w-full"
         >
-            <div className=":w-full mb:w-1/4 h-96 p-5 bg-black-100 rounded-2xl border-4 border-neutral-800 flex flex-col items-start space-y-5 hover:border-secondary my-5 md:m-9 transition duration-300 ease-in-out transform hover:scale-105">
-                <div className="text-6xl text-white"><IconComponent /></div>
-                <div className="text-center text-gray-400 text font-normal">{title}</div>
-                <div className="text-gray-400 text-xl font-normal">{description}</div>
+            <div className=":w-full mb:w-1/4 h-96 p-1 bg-black-100 rounded-xl flex flex-col items-start space-y-5 hover:border-secondary my-5 md:m-9 transition duration-300 ease-in-out transform hover:scale-105 bg-gradient-to-tr from-neutral-800 to-neutral-500  hover:from-secondary hover:to-primary hover:shadow-2xl">
+                <div className="flex flex-col items-start bg-black-100 p-3 rounded-lg w-full h-full ">
+                    <div className="text-6xl text-white my-2"><IconComponent /></div>
+                    <div className="text-center text-gray-400 text font-normal my-4">{title}</div>
+                    <div className="text-gray-400 text-xl font-normal my-4">{description}</div>
+                </div>
             </div>
         </motion.div>
     );
